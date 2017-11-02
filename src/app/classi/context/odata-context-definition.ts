c0fc95a11f0dd69c95a4fb1013ce43bebcf1a1d4
g.demarco <devnull@localhost>
2017-11-02 15:00:55 +0100

    refacoring

--- src/app/classi/context/odata-context-definition.ts
+++ src/app/classi/context/odata-context-definition.ts
@@ -13,8 +13,6 @@ import {AfferenzaStruttura} from "../entities/afferenza-struttura";
 import {TipoProcedimento} from "../entities/tipo-procedimento";
 import {AziendaTipoProcedimento} from "../entities/azienda-tipo-procedimento";
 import {Titolo} from "../entities/titolo";
-import {isArray} from "rxjs/util/isArray";
-import {CustomLoadingFilterParams} from "../custom-loading-filter-params";
 import {Ruolo} from "../entities/ruolo";
 
 @Injectable()
@@ -80,47 +78,6 @@ private odataContext:ODataContext;
             }
         }
         console.log(values);
-    }
-
-    public customLoading (loadOption: any) {
-        const customLoadingFilterParams: CustomLoadingFilterParams = loadOption.userData["customLoadingFilterParams"]
-        // console.log("custom", customLoadingFilterParams);
-        // console.log("option", loadOption);
-        if (loadOption.filter != null && customLoadingFilterParams != null) {
-            const currentFilter = loadOption.filter;
-            for (const i in currentFilter) {
-                const targetField: string = customLoadingFilterParams.getTargetField();
-                let currentField: any;
-                let currentValue: any;
-                if (isArray(currentFilter[i][0])) {
-                    currentField = currentFilter[i][0][0];
-                    currentValue = currentFilter[i][0][2];
-                } else {
-                    currentField = currentFilter[i][0];
-                    currentValue = currentFilter[i][2];
-                }
-                if (currentField === targetField) {
-                    let myFilter: any = customLoadingFilterParams.getFilter();
-                    myFilter = this.buildFilter(myFilter, targetField, currentValue);
-                    currentFilter[i] = myFilter;
-                    break;
-                }
-            }
-        }
-    }
-
-    private buildFilter(filter: any, target: string, value: any) {
-        let stringFilter = JSON.stringify(filter);
-        stringFilter = stringFilter.replace("${target}", target);
-        if (typeof(value) === "string") {
-            stringFilter = stringFilter.replace(/\${value}/g, value);
-            stringFilter = stringFilter.replace(/\${value.tolower}/g, value.toLowerCase());
-        } else {
-            stringFilter = stringFilter.replace(/"\${value}"/g, value);
-            stringFilter = stringFilter.replace(/"\${value.tolower}"/g, value);
-        }
-        // console.log("stringFilter", stringFilter);
-        return JSON.parse(stringFilter);
     }
 
     private setCustomConfiguration() {
