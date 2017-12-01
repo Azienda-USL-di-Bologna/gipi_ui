8002061cc7a675b1f96109cf47301a464abd73b1
l.salomone <devnull@localhost>
2017-12-01 13:14:31 +0100

    migliorato il sistema per portare la userInfoMap dal login nel sessionStorage dati visualizzabili dall'utente nella Home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,6 +3,12 @@ import {DEFAULT_INTERRUPTSOURCES, Idle} from "@ng-idle/core";
 import {Keepalive} from "@ng-idle/keepalive";
 import {Router} from "@angular/router";
 import {SessionManager} from "../login/session-manager";
+import { SharedData } from '@bds/nt-angular-context/shared-data';
+import { log } from 'util';
+import { Ruolo } from 'app/classi/server-objects/entities/ruolo';
+import { Azienda } from 'app/classi/server-objects/entities/azienda';
+import { Struttura } from 'app/classi/server-objects/entities/struttura';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-home',
@@ -10,9 +16,28 @@ import {SessionManager} from "../login/session-manager";
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  private userInfoMap: Object;
+  private username: String;
+  private ruolo: Ruolo;
+  private azienda: Azienda;
+  private strutture: Struttura[];
+  private struttureDiSeguito = "";
 
   constructor(private sessionManager: SessionManager){
     sessionManager.setExpireTokenOnIdle(300);
+    this.userInfoMap = JSON.parse(sessionStorage.getItem("userInfoMap"));
+    console.log("Vediamo se suca: ",this.userInfoMap)
+
+    this.username = this.userInfoMap["username"];
+    this.azienda = this.userInfoMap["azienda"];
+    this.strutture = this.userInfoMap["strutture"];
+    this.ruolo = this.userInfoMap["ruolo"];
+
+    this.strutture.forEach(element => {
+      this.struttureDiSeguito = this.struttureDiSeguito + ";";
+    });
+    this.struttureDiSeguito = this.struttureDiSeguito == "" ? "Nessuna struttura associata" : this.struttureDiSeguito;
+
   }
   ngOnInit() {
   }
