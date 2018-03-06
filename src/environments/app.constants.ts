dea4469e916895568e3d843a07451bef9ef8313b
Andrea Zucchelli <andrea.zucchelli@cup2000.it>
2018-03-06 11:16:56 +0100

    Spostato path di chiamata ai servizi da /gipi a /gipi-api

--- src/environments/app.constants.ts
+++ src/environments/app.constants.ts
@@ -8,16 +8,16 @@ const port: string = hostname === "localhost" ? ":10006" : ":443";
 console.log("hostname", hostname);
 console.log("port", port);
 
-export const ODATA_BASE_RELATIVE_URL: string = "/gipi/resources/odata.svc";
+export const ODATA_BASE_RELATIVE_URL: string = "/gipi-api/resources/odata.svc";
 export const ODATA_BASE_URL: string = window.location.protocol + "//" + hostname + port + ODATA_BASE_RELATIVE_URL;
-export const CUSTOM_RESOURCES_RELATIVE_URL: string = "/gipi/resources/custom/";
+export const CUSTOM_RESOURCES_RELATIVE_URL: string = "/gipi-api/resources/custom/";
 export const CUSTOM_RESOURCES_BASE_URL: string = window.location.protocol + "//" + hostname + port + CUSTOM_RESOURCES_RELATIVE_URL;
 
 
 // login
 export const LOGOUT_RELATIVE_URL: string = "/Shibboleth.sso/Logout";
 export const LOGOUT_URL = window.location.protocol + "//" + hostname + port + LOGOUT_RELATIVE_URL;
-export const LOGIN_RELATIVE_URL: string = "/gipi/user/login/";
+export const LOGIN_RELATIVE_URL: string = "/gipi-api/user/login/";
 export const LOGIN_URL: string = window.location.protocol + "//" + hostname + port + LOGIN_RELATIVE_URL;
 
 export const LOGIN_ROUTE: string = "/login";
@@ -28,8 +28,8 @@ export const HOME_ROUTE: string = "/home";
 console.log("ODATA_BASE_URL", ODATA_BASE_URL);
 console.log("CUSTOM_RESOURCES_BASE_URL", CUSTOM_RESOURCES_BASE_URL);
 console.log("LOGIN_URL", LOGIN_URL);
-console.log("1", window.location.protocol + "//" + hostname + port + "/gipi/resources/odata.svc");
-console.log("2", window.location.protocol + "//" + hostname + "/gipi/resources/odata.svc");
+console.log("1", window.location.protocol + "//" + hostname + port + "/gipi-api/resources/odata.svc");
+console.log("2", window.location.protocol + "//" + hostname + "/gipi-api/resources/odata.svc");
 
 export const DEFAULT_TIMEZONE_OFFSET = 0;
 export const DEFAULT_CURRENCY = "EUR";
