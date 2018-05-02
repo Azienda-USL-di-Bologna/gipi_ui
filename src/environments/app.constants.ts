bce652f8808f1ca2aa56bd0522c4464660738ca8
g.demarco <devnull@localhost>
2018-05-02 15:27:23 +0200

    fix su X di chiusura in modalità Simple per poter inserire anche il cambiamento di icona

--- src/environments/app.constants.ts
+++ src/environments/app.constants.ts
@@ -54,8 +54,7 @@ export const odataStrutturePath = "/Strutturas";
 export const BarsMode = {ALL: "all", NONE: "none", SIMPLE: "simple", SIMPLE_WITH_SIDEBAR: "simple_with_sidebar"};
 
 export const GlobalContextVariable = {
-    FIRST_ACCESS_PATH: "firstAccessPath",
-    CURRENT_PATH: "currentPath"
+    ACCESS_PATHS_ARRAY: "accessPathsArray",
 };
 
 // ======================= ENUM ========================================
