df82042780eedd5ae03376cba7077b50901ae5df
l.salomone <devnull@localhost>
2018-05-03 10:19:33 +0200

    giusto uno spazio

--- src/app/cambio-di-stato-box/cambio-di-stato-box.component.ts
+++ src/app/cambio-di-stato-box/cambio-di-stato-box.component.ts
@@ -30,7 +30,7 @@ export class CambioDiStatoBoxComponent implements OnInit {
   public showPopupAnnullamento: boolean = false;
   public dataSourceStati: DataSource;
   public dataIniziale: Date;
-  public arrayEsiti: any[] = Object.keys(ESITI).map(key => {return {"codice": key, "descrizione": ESITI[key]};});
+  public arrayEsiti: any[] = Object.keys(ESITI).map(key => {return {"codice": key, "descrizione": ESITI[key]}; });
   public arrayRiassunto: PopupRow[];
   public loadingVisible: boolean = false;
 
